1071fc3ae28d7b2cec7fda08953db4478ca2a058
Max Battcher <me@worldmaker.net>
2015-08-11 23:53:57 -0400

    :wrench: Ctrl+Shift+B (oops)

--- loader.js
+++ loader.js
@@ -1,8 +1,8 @@
-let jadeVirtualdomPath = System.normalize('jade-virtualdom');
+let normalPromise = System.normalize('jade-virtualdom');
 export function translate(load) {
-    return `
+    return normalPromise.then(jadeVirtualdomPath => `
 		import jadeVirtualdom from '${jadeVirtualdomPath}';
 		export default let template = jadeVirtualdom(${JSON.stringify(load.source)});
-	`;
+	`);
 }
 //# sourceMappingURL=loader.js.map
